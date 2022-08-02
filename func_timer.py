4f99e664c14fb6aed81298a5954e73f40020aaeb
Pablo Saldivia <pablosaldiviainfo@gmail.com>
2022-08-01 22:34:31 -0300

    Primer commit de la función timer. Se le puee agregar cualquier función decorandola con un @ invocando al timer

---
+++ func_timer.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def tiempo_total(func):
+    def wrapper():
+        initial_time = datetime.now()
+        func()
+        final_time = datetime.now()
+        time_elapsed = final_time - initial_time
+        print(f"Pasaron {time_elapsed.total_seconds()} segundos")
+    return wrapper
+
+@tiempo_total
+def random_func():
+    for _ in range(1, 1000000):
+        pass
+
+random_func()
